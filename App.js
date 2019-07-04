17de13e1d992ad673eb3457642cf44b30c136041
Eric Kwon <ericyong95@naver.com>
2019-07-04 21:53:58 +0900

    Added input style and list scroll function

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import ToDo from './ToDo'
 
 const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component{
+  state = {
+    newToDo: ""
+  }
   render(){
+    const { newToDo } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barstyle="light-content"/>
         <Text style={styles.title}>Kawai Todo</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"}/>
+          <TextInput 
+            style={styles.input} 
+            placeholder={"New To Do"} 
+            value={newToDo} 
+            onChangeText={this._controlNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView>
+            <ToDo></ToDo>
+          </ScrollView>
         </View>
       </View>
     )
   }
+  _controlNewToDo = text => {
+    this.setState({
+      newToDo: text
+  })};
 }
 
 const styles = StyleSheet.create({
@@ -38,7 +59,7 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 10,
     ...Platform.select({
       ios:{
-        shadowColor:"rgb(50, 50, 50)",
+        shadowColor: "rgb(50, 50, 50)",
         shadowOpacity: 0.5,
         shadowRadius: 5,
         shadowOffset:{
@@ -52,6 +73,9 @@ const styles = StyleSheet.create({
     })
   },
   input: {
-
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 20
   }
 });
---
+++ ToDo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+class ToDo extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>안녕 나는 할일목록</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({})
+
+export default ToDo
